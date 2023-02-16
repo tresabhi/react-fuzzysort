4a43ac9757c2371f13685772f57b35b81c27b803
tresabhi <coolabhigyaandeep07@gmail.com>
2023-02-16 15:21:59 +0000

    feat: storybook and new events

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,11 +1,129 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Search } from ".";
+import { faker } from '@faker-js/faker';
+import { ComponentMeta } from '@storybook/react';
+import { times } from 'lodash';
+import { useRef } from 'react';
+import { Search, SearchItem } from '.';
 
 export default {
+  title: 'Search',
   component: Search,
 } as ComponentMeta<typeof Search>;
 
-const Template: ComponentStory<typeof Search> = (args) => <Search {...args} />;
+function simpleList(size: number, generator: () => string) {
+  const list: SearchItem[] = [];
 
-export const Primary = Template.bind({});
-Primary.args = {};
+  times(size, () => {
+    const item = generator();
+
+    list.push({
+      node: <p>{item}</p>,
+      query: item,
+    });
+  });
+
+  return list;
+}
+
+export function Addresses1000() {
+  const input = useRef<HTMLInputElement>(null);
+  const list = simpleList(1000, faker.address.streetAddress);
+
+  return (
+    <>
+      <input ref={input} />
+      <Search input={input} list={list} />
+    </>
+  );
+}
+
+export function Names1000() {
+  const input = useRef<HTMLInputElement>(null);
+  const list = simpleList(1000, faker.name.fullName);
+
+  return (
+    <>
+      <input ref={input} />
+      <Search input={input} list={list} />
+    </>
+  );
+}
+
+export function Fallback() {
+  const input = useRef<HTMLInputElement>(null);
+  const list = simpleList(5, faker.name.fullName);
+
+  return (
+    <>
+      <input ref={input} />
+      <Search
+        input={input}
+        list={list}
+        fallback={<p style={{ color: 'gray' }}>No results found</p>}
+      />
+    </>
+  );
+}
+
+export function Debounce() {
+  const input = useRef<HTMLInputElement>(null);
+  const list = simpleList(5, faker.name.fullName);
+
+  return (
+    <>
+      <input ref={input} />
+      <Search input={input} list={list} debounce={500} />
+    </>
+  );
+}
+
+export function OnFirstItemChange() {
+  const input = useRef<HTMLInputElement>(null);
+  const paragraph = useRef<HTMLParagraphElement>(null);
+  const list = simpleList(5, faker.name.fullName);
+
+  return (
+    <>
+      <p ref={paragraph}>First item: {list[0].query}</p>
+
+      <hr />
+
+      <input ref={input} />
+      <Search
+        input={input}
+        list={list}
+        onFirstItemChange={(item) => {
+          if (paragraph.current) {
+            paragraph.current.innerText = `First item: ${item?.query}`;
+          }
+        }}
+      />
+    </>
+  );
+}
+
+export function OnNoResults() {
+  const input = useRef<HTMLInputElement>(null);
+  const paragraph = useRef<HTMLParagraphElement>(null);
+  const list = simpleList(5, faker.name.fullName);
+  let count = 0;
+
+  return (
+    <>
+      <p ref={paragraph}>Number of times there have been no results: 0</p>
+
+      <hr />
+
+      <input ref={input} />
+      <Search
+        input={input}
+        list={list}
+        onNoResults={() => {
+          if (paragraph.current) {
+            count++;
+            paragraph.current.innerText = `Number of times there have been no results: ${count}`;
+          }
+        }}
+      />
+    </>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
-import { go } from "fuzzysort";
-import debounce from "lodash.debounce";
-import {
-  Fragment,
-  ReactNode,
-  RefObject,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { go } from 'fuzzysort';
+import debounce from 'lodash.debounce';
+import { Fragment, ReactNode, RefObject, useEffect, useState } from 'react';
 
 /**
  * An item that can be searched through
@@ -16,18 +9,17 @@ export type SearchItem = {
   /**
    * The string that is used to both discriminate and search an item
    */
-  string: string;
+  query: string;
   /**
-   * The thing that gets rendered
+   * The thing that gets rendered;
+   * [don't forget to add keys](https://reactjs.org/docs/lists-and-keys.html)!
    */
   node: ReactNode;
-  /**
-   * This is called when enter is clicked when this item is the first in the
-   * list
-   */
-  callback?: () => void;
 };
 
+/**
+ * Props for configuring the behavior of Search
+ */
 export interface SearchProps {
   /**
    * A list of items that can be searched through
@@ -42,74 +34,90 @@ export interface SearchProps {
    */
   fallback?: ReactNode;
   /**
-   * If escape is clicked, this will run
-   */
-  escape?: () => void;
-  /**
    * The debounce time to avoid excessive rerenders
    */
   debounce?: number;
+  /**
+   * Called when the first item on the list changes to a different item; the
+   * item argument will be undefined when there are no results
+   */
+  onFirstItemChange?: (item?: SearchItem) => void;
+  /**
+   * Called when there are no results
+   */
+  onNoResults?: () => void;
+  /**
+   * Options for the fuzzysort library that is used under the hood
+   *
+   * [Fuzzysort documentation](https://github.com/farzher/fuzzysort#readme)
+   */
+  keyOptions?: Omit<Fuzzysort.KeyOptions, 'key'>;
 }
 
 /**
  * Searches through a list of nodes based on the value on input powered by
  * the fuzzysort library
  */
-export  function Search({
+export function Search({
   list,
   input,
   fallback,
-  escape,
   debounce: debounceTime = 0,
+  onFirstItemChange,
+  onNoResults,
+  keyOptions,
 }: SearchProps) {
   const [defaultList] = useState<ReactNode[]>(
-    list.map(({ node, string }) => <Fragment key={string}>{node}</Fragment>)
+    list.map(({ node, query: string }) => (
+      <Fragment key={string}>{node}</Fragment>
+    )),
   );
   const [children, setChildren] = useState(defaultList);
-  const firstCallback = useRef<(() => void) | undefined>(list[0]?.callback);
+  let previousFirstItem: SearchItem | undefined = undefined;
 
   const handleChange = debounce((event: Event) => {
     const { value } = event.target as HTMLInputElement;
 
-    if (value === "") {
+    if (value === '') {
       setChildren(defaultList);
+      if (onFirstItemChange) onFirstItemChange(list[0]);
     } else {
-      const results = go(value, list, { key: "string" });
+      const results = go(value, list, { ...keyOptions, key: 'query' });
       const resultNodes = results.map((result, index) => {
-        if (index === 0) firstCallback.current = result.obj.callback;
         return result.obj.node;
       });
 
-      if (resultNodes.length === 0) {
-        firstCallback.current = undefined;
-
+      if (results.length === 0) {
         if (fallback) {
           setChildren([fallback]);
         } else {
           setChildren([]);
         }
+
+        if (onNoResults) onNoResults();
+        if (onFirstItemChange) onFirstItemChange(undefined);
+
+        previousFirstItem = undefined;
       } else {
         setChildren(resultNodes);
+
+        const firstItem = results[0].obj;
+
+        if (onFirstItemChange && previousFirstItem !== firstItem) {
+          onFirstItemChange(firstItem);
+          previousFirstItem = firstItem;
+        }
       }
     }
   }, debounceTime);
-  const handleKeydown = (event: Event) => {
-    if ((event as KeyboardEvent).key === "Enter") {
-      if (firstCallback.current) firstCallback.current();
-    } else if ((event as KeyboardEvent).key === "Escape") {
-      if (escape) escape();
-    }
-  };
 
   useEffect(() => {
     const copiedInput = input.current;
 
-    copiedInput?.addEventListener("input", handleChange);
-    copiedInput?.addEventListener("keydown", handleKeydown);
+    copiedInput?.addEventListener('input', handleChange);
 
     return () => {
-      copiedInput?.removeEventListener("input", handleChange);
-      copiedInput?.removeEventListener("keydown", handleKeydown);
+      copiedInput?.removeEventListener('input', handleChange);
     };
   });
 
